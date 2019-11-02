7a508cc769b47d17ecb400a0195a5a1a037e27ff
Blair Noctis <code@noctis.im>
2019-11-02 21:36:10 +0800

    fix: remove accidently added files

--- ohmydomains/registrars/dynadot.py
+++
@@ -1,7 +0,0 @@
-from ohmydomains.registrars.account import RegistrarAccount
-
-
-class DynadotAccount(RegistrarAccount):
-	API_BASE = 'https://api.dynadot.com/api3.xml'
-	NEEDED_CREDENTIALS = ('api_key',)
-
